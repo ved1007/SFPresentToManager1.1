fd91f78ed10dd191e4fbe4679933256205af5e4f
ved1007 <ved1007@gmail.com>
2020-12-08 16:23:18 -0600

    Added Present to Manager

---
+++ src/main/java/myTestRunner/PresentToManagerRunner.java
@@ -0,0 +1,23 @@
+package myTestRunner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "/Users/vedant.jagani/Desktop/Salesforce/CucumberBDDFrameVedant/src/main/java/Features/PresentToManager.feature",
+
+        glue = {"stepDefinitions2" }, 
+        plugin = { "pretty", "html:test-outout", "json:json_output/cucumber.json",
+				"junit:junit_xml/cucumber.xml" },
+		monochrome = true,
+		strict = true,
+		dryRun = false
+//      tags = {"~@SmokeTest" , "~@RegressionTest", "~@End2End"}
+	
+)
+
+public class PresentToManagerRunner {
+
+}
