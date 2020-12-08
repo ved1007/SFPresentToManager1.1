9e8f417a013f1a589cd45a8ba18f0df61201a283
ved1007 <ved1007@gmail.com>
2020-12-08 16:32:28 -0600

    Updated Present to manager Runner file 2nd time

--- src/main/java/myTestRunner/PresentToManagerRunner.java
+++ src/main/java/myTestRunner/PresentToManagerRunner.java
@@ -8,14 +8,9 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "/Users/vedant.jagani/Desktop/Salesforce/CucumberBDDFrameVedant/src/main/java/Features/PresentToManager.feature",
 
-        glue = {"stepDefinitions2" }, 
-        plugin = { "pretty", "html:test-outout", "json:json_output/cucumber.json",
-				"junit:junit_xml/cucumber.xml" },
-		monochrome = true,
-		strict = true,
-		dryRun = false
+		glue = { "stepDefinitions2" }, plugin = { "pretty", "html:test-outout", "json:json_output/cucumber.json",
+				"junit:junit_xml/cucumber.xml" }, monochrome = true, strict = true, dryRun = false
 
-	
 )
 
 public class PresentToManagerRunner {
